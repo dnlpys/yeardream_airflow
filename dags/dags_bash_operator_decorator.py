1fa6657a66f0298d01f032fc5151bfbc9169fdf0
dnlpys <dnlpys9999@gmail.com>
2024-06-17 10:31:13 +0900

    add dags/dags_python_operator.py

--- dags/dags_bash_operator_decorator.py
+++ dags/dags_bash_operator_decorator.py
@@ -1,16 +1,13 @@
-from airflow import DAG
-import datetime
+from airflow.decorators import dag
 import pendulum
 from airflow.operators.bash import BashOperator
-from airflow.operators.empty import EmptyOperator
-from airflow.decorators import dag
 
-@dag(dag_id="dags_bash_operator_decorator",
-     start_date=datetime.datetime(2024, 6, 1, tz="Asia/Seoul"),
+@dag(dag_id='dags_bash_operator_decorator',
      schedule="0 13 * * 5#2",
+     start_date=pendulum.datetime(2024, 6, 1, tz="Asia/Seoul"),
      catchup=False,
-     tag=['homework'])
-
+     tags=['homework']
+)
 def dags_bash_operator_decorator():
     bash_t1 = BashOperator(
         task_id="bash_t1",
@@ -25,28 +22,3 @@ def dags_bash_operator_decorator():
     bash_t1 >> bash_t2
 
 dags_bash_operator_decorator()
-
-
-# with DAG(
-#     dag_id="dags_bash_operator",
-#     schedule="0 0 * * *",
-#     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
-#     catchup=False
-# ) as dag:
-#     bash_t1 = BashOperator(
-#         task_id="bash_t1",
-#         bash_command="echo whoami",
-#     )
-#
-#     bash_t2 = BashOperator(
-#         task_id="bash_t2",
-#         bash_command="echo $HOSTNAME",
-#     )
-
-
-
-
-
-
-
-
